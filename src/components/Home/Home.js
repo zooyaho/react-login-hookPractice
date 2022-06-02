a492152683ad96375b8d6efbbc2a0e24ce1d5f44
zooyaho <jiwoo1268@naver.com>
2022-06-02 23:20:28 +0900

    설명 추가 주석처리

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,7 @@ const Home = (props) => {
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
       <Button onClick={props.onLogout}>Logout</Button>
+      {/* Button은 프레젠테이션 컴포넌트이기때문에 useContext사용해서 핸들러를 등록시킬 필요없음 -> 바인딩하면 이 버튼은 항상 로그아웃만 되기때문에!! */}
     </Card>
   );
 };
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: ()=>{}, // 더미 함수를 작성하면 IDE자동완성을 더 좋게 만듬~!
 });
 // 빈 state의 컴포넌트 생성
 // 보통 {}객체를 사용, 그러므로 객체를 갖는 컴포넌트를 반환함.
